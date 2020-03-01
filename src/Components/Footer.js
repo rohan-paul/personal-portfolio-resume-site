93288a584bcbc4ce7343fa0a6ade6cc35a2098dc
Rohan Paul <rohanpaul2@gmail.com>
2020-03-02 01:21:34 +0530

    upadated link rendering and resume source document

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class Footer extends Component {
   render() {
-
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function(network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url} target="_blank">
+              <i className={network.className}></i>
+            </a>
+          </li>
+        )
       })
     }
 
     return (
       <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Rohan Paul</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
+            <ul className="copyright">
+              <li>&copy; Copyright 2020 Rohan Paul</li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a
+              className="smoothscroll"
+              title="Back to Top"
+              href="#home"
+              target="_blank"
+            >
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
-    );
+      </footer>
+    )
   }
 }
 
-export default Footer;
+export default Footer
